597cce953333e9c7f4b7e229ede92cd64a999b0a
harun ulum <student.harun0813@gmail.com>
2021-05-21 01:15:52 +0700

    change model and response in AI

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 import os
-
-from fastapi import FastAPI, UploadFile, File
-import cv2
+import numpy as np
 import uuid
 import shutil
 import tensorflow as tf
+from fastapi import FastAPI, UploadFile, File
+from keras.preprocessing import image
 
 app = FastAPI()
 model_skin_classifier_cnn = tf.keras.models.load_model('skin_cancer.h5')
@@ -29,16 +29,18 @@ def read_root(skin_image: UploadFile = File(...)):
 
     generate_uuid = uuid.uuid4().hex
     file_name_uuid = '{}_{}'.format(generate_uuid, skin_image.filename)
-    with open('{}/{}'.format(base_dir, file_name_uuid), 'wb') as image:
-        shutil.copyfileobj(skin_image.file, image)
+    path = '{}/{}'.format(base_dir, file_name_uuid)
+    with open(path, 'wb') as img:
+        shutil.copyfileobj(skin_image.file, img)
 
-    img = cv2.imread('{}/{}'.format(base_dir, file_name_uuid))
-    img = cv2.resize(img, (28, 28))
-    result = model_skin_classifier_cnn.predict(img.reshape(1, 28, 28, 3))
+    img = image.load_img(path, target_size=(28, 28))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = np.vstack([x])
+    result = model_skin_classifier_cnn.predict(x)
     max_prob = max(result[0])
-    class_ind = list(result[0]).index(max_prob)
-    class_name = cancer_class[class_ind]
-
-    os.remove('{}/{}'.format(base_dir, file_name_uuid))
+    class_index = list(result[0]).index(max_prob)
+    class_name = cancer_class[class_index]
+    os.remove(path)
 
     return custom_res(True, file_name_uuid, class_name)
